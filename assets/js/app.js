0f2e07e3bd1d2ca7bb70c05fa1a842eac37d2ddb
Decheng Zhang <qrafzv88@gmail.com>
2018-09-24 12:29:42 -0400

    add a new popover type popover-string

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,53 +2,81 @@ define([
        "jquery" , "underscore" , "backbone"
        , "collections/snippets" , "collections/my-form-snippets"
        , "views/tab" , "views/my-form"
-       , "text!data/n2.json", "text!data/input.json", "text!data/radio.json", "text!data/select.json", "text!data/buttons.json"
+       , "text!data/input.json", "text!data/radio.json", "text!data/select.json", "text!data/buttons.json"
        , "text!templates/app/render.html",  "text!templates/app/about.html", 
 ], function(
   $, _, Backbone
   , SnippetsCollection, MyFormSnippetsCollection
   , TabView, MyFormView
-  , n2mandatoryJSON, inputJSON, radioJSON, selectJSON, buttonsJSON
+  , inputJSON, radioJSON, selectJSON, buttonsJSON
   , renderTab, aboutTab
 ){
   return {
     initialize: function(){
 
       //Bootstrap tabs from json.
-     /* new TabView({
+      new TabView({
         title: "Input"
         , collection: new SnippetsCollection(JSON.parse(inputJSON))
       });
       new TabView({
         title: "Radios / Checkboxes"
         , collection: new SnippetsCollection(JSON.parse(radioJSON))
-      });*/
+      });
       new TabView({
         title: "Select"
         , collection: new SnippetsCollection(JSON.parse(selectJSON))
-       });
-	/*
+      });
       new TabView({
         title: "Buttons"
         , collection: new SnippetsCollection(JSON.parse(buttonsJSON))
-      });*/
+      });
       new TabView({
         title: "Rendered"
         , content: renderTab
-      });/*
+      });
       new TabView({
         title: "About"
         , content: aboutTab
       });
-*/
+
       //Make the first tab active!
       $("#components .tab-pane").first().addClass("active");
       $("#formtabs li").first().addClass("active");
       // Bootstrap "My Form" with 'Form Name' snippet.
       new MyFormView({
         title: "Original"
-        , collection: new MyFormSnippetsCollection(JSON.parse(n2mandatoryJSON))
+        , collection: new MyFormSnippetsCollection([
+          { "title" : "Form Name"
+            , "fields": {
+              "name" : {
+                "label"   : "Form Name"
+                , "type"  : "input"
+                , "value" : "Form Name"
+              }
+            }
+          }
+        ])
       });
+
+      new MyFormView({
+	    title: "Attribute"
+	    , collection : new MyFormSnippetsCollection([
+		{
+		    "title": "Attribute List"
+		    ,"fields" : {
+			"name": {
+			    "label"   : "Attributes"
+			    , "type"  : "string"
+			    , "value" : "attributes"
+			}
+		    }
+		    
+
+		}
+	    ])
+
+	    })
     }
   }
 });
--- assets/js/views/snippet.js
+++ assets/js/views/snippet.js
@@ -5,8 +5,9 @@ define([
   , "text!templates/popover/popover-select.html"
   , "text!templates/popover/popover-textarea.html"
   , "text!templates/popover/popover-textarea-split.html"
-  , "text!templates/popover/popover-checkbox.html"
-    , "templates/snippet/snippet-templates" 
+    , "text!templates/popover/popover-checkbox.html"
+    , "text!templates/popover/popover-string.html"
+  , "templates/snippet/snippet-templates"
   , "bootstrap"
 ], function(
   $, _, Backbone
@@ -15,20 +16,22 @@ define([
   , _PopoverSelect
   , _PopoverTextArea
   , _PopoverTextAreaSplit
-  , _PopoverCheckbox
+    , _PopoverCheckbox
+    , _PopoverString
   , _snippetTemplates
 ){
   return Backbone.View.extend({
     tagName: "div"
     , className: "component" 
     , initialize: function(){
-      this.template = _.template(_snippetTemplates[this.model.idFriendlyTitle()+ "html"])
+      this.template = _.template(_snippetTemplates[this.model.idFriendlyTitle()])
       this.popoverTemplates = {
         "input" : _.template(_PopoverInput)
         , "select" : _.template(_PopoverSelect)
         , "textarea" : _.template(_PopoverTextArea)
         , "textarea-split" : _.template(_PopoverTextAreaSplit)
-        , "checkbox" : _.template(_PopoverCheckbox)
+          , "checkbox" : _.template(_PopoverCheckbox)
+	  ,"string" : _.template(_PopoverString)
       }
     }
     , render: function(withAttributes){
@@ -38,15 +41,11 @@ define([
         "items" : that.model.get("fields"),
         "popoverTemplates": that.popoverTemplates
       });
-	var jsonContent =  _.template(_snippetTemplates[this.model.idFriendlyTitle()])(that.model.getValues());
-	
-	
-	if (withAttributes) {
+      if (withAttributes) {
         return this.$el.html(
-            that.template(that.model.getValues())
+          that.template(that.model.getValues())
         ).attr({
-           "data-content"     : content
-         ,"json-content"    : jsonContent
+          "data-content"     : content
           , "data-title"     : that.model.get("title")
           , "data-trigger"   : "manual"
           , "data-html"      : true
